603f77227064dc10b285dcf58112e60201d041cd
SANdhya0207 <jhas54212@gmail.com>
2023-07-21 17:44:33 +0530

    complete

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require ("express");
+const morgan = require ("morgan")
+const color = require ("colors")
+const dotenv = require ("dotenv")
+const cors = require ("cors");
+const connectDB = require("./configDB/db");
+const route = require("./Routes/authRoutes");
+const path = require('path')
+
+// Environment Variables file configuration
+dotenv.config();
+
+connectDB();
+
+// Creating Object of express
+const app = express();
+
+// Using Express, morgan and cors
+app.use(express.json());
+// Middleware helps in Logging HTTP request details such as the request method, url, response satus and response time and size
+app.use(morgan('dev'));
+// Cross Origin Access provide karta hain
+app.use(cors());
+// adding path middleware
+app.use(express.static(path.join(__dirname, './my-app/build')));
+
+
+// Creating route for accessing 
+app.use('/api/v1/auth',route);
+
+app.use('*', (req, res) => {
+      res.sendFile(path.join(__dirname, './my-app/build/index.html'));
+})
+
+const Port = process.env.PORT || 6000
+
+app.listen(Port, () => {
+      console.log(`Listening on port ${Port}`.bgCyan);
+})
